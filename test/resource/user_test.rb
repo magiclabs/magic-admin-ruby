37131b831e21e08d9b3e0a40ea8373390c24c470
Justin Herrera <justin@magic.link>
2022-12-05 10:39:02 -0800

    refactor logic and tests

--- test/resource/user_test.rb
+++ test/resource/user_test.rb
@@ -26,35 +26,35 @@ describe MagicAdmin::Resource::User do
     expect(subject).to respond_to(:magic)
   end
 
-  context "#get_metadata_by_issuer" do
-    it "send request with options" do
+  describe "#get_metadata_by_issuer with or without wallet" do
+    before(:each) do
       allow(MagicAdmin::Util).to receive(:headers)
-        .with(magic.secret_key)
-        .and_return({})
-      expect(magic.http_client).to receive(:call)
-        .with(:get,
-              "/v1/admin/auth/user/get",
-              {
-                params: { issuer: issuer, wallet_type: MagicAdmin::Resource::WalletType::NONE }, headers: {}
-              })
-
-      subject.get_metadata_by_issuer(issuer)
+      .with(magic.secret_key)
+      .and_return({})
     end
-  end
 
-  context "#get_metadata_by_issuer_and_wallet" do
-    it "send request with options" do
-      allow(MagicAdmin::Util).to receive(:headers)
-        .with(magic.secret_key)
-        .and_return({})
-      expect(magic.http_client).to receive(:call)
-        .with(:get,
-              "/v1/admin/auth/user/get",
-              {
-                params: { issuer: issuer, wallet_type: MagicAdmin::Resource::WalletType::ALGOD }, headers: {}
-              })
+    context "#get_metadata_by_issuer no wallet" do
+      it "send request with options" do
+        expect(magic.http_client).to receive(:call)
+          .with(:get,
+                "/v1/admin/auth/user/get",
+                {
+                  params: { issuer: issuer, wallet_type: MagicAdmin::Resource::WalletType::NONE }, headers: {}
+                })
+        subject.get_metadata_by_issuer(issuer)
+      end
+    end
 
-      subject.get_metadata_by_issuer_and_wallet(issuer, MagicAdmin::Resource::WalletType::ALGOD)
+    context "#get_metadata_by_issuer with wallet" do
+      it "send request with options" do
+        expect(magic.http_client).to receive(:call)
+          .with(:get,
+                "/v1/admin/auth/user/get",
+                {
+                  params: { issuer: issuer, wallet_type: MagicAdmin::Resource::WalletType::ALGOD }, headers: {}
+                })
+        subject.get_metadata_by_issuer(issuer, MagicAdmin::Resource::WalletType::ALGOD)
+      end
     end
   end
 
@@ -70,14 +70,14 @@ describe MagicAdmin::Resource::User do
     end
   end
 
-  context "#get_metadata_by_public_address_and_wallet" do
+  context "#get_metadata_by_public_address" do
     it "return response" do
       url = "https://api.magic.link/v1/admin/auth/user/get?issuer="
       url += construct_issuer_with_public_address
       url += "&wallet_type=" + MagicAdmin::Resource::WalletType::SOLANA
       stub_request(:get, url)
         .to_return(status: 200, body: stub_response_body.to_json, headers: {})
-      reps = subject.get_metadata_by_public_address_and_wallet(public_address, MagicAdmin::Resource::WalletType::SOLANA)
+      reps = subject.get_metadata_by_public_address(public_address, MagicAdmin::Resource::WalletType::SOLANA)
       expect(reps.status_code).to eq(200)
     end
   end
@@ -95,47 +95,44 @@ describe MagicAdmin::Resource::User do
     end
   end
 
-  context "#get_metadata_by_token_and_wallet" do
+  context "#get_metadata_by_token" do
     it "return response" do
       url = "https://api.magic.link/v1/admin/auth/user/get?issuer="
       url += issuer
       url += "&wallet_type=" + MagicAdmin::Resource::WalletType::ANY
       stub_request(:get, url)
         .to_return(status: 200, body: stub_response_body.to_json, headers: {})
-      reps = subject.get_metadata_by_token_and_wallet(spec_did_token, MagicAdmin::Resource::WalletType::ANY)
+      reps = subject.get_metadata_by_token(spec_did_token, MagicAdmin::Resource::WalletType::ANY)
 
       expect(reps.status_code).to eq(200)
     end
   end
 
-  context "#logout_by_issuer" do
-    it "return response" do
+  describe "#get_metadata_by_issuer object network strategy" do
+    before(:each) do
       url = "https://api.magic.link/v2/admin/auth/user/logout"
-      stub_request(:post, url)
-        .to_return(status: 200, body: stub_response_body.to_json, headers: {})
-      reps = subject.logout_by_issuer(issuer)
-
-      expect(reps.status_code).to eq(200)
+      stub_request(:post, url).to_return(status: 200, body: stub_response_body.to_json, headers: {})
     end
-  end
 
-  context "#logout_by_public_address" do
-    it "return response" do
-      url = "https://api.magic.link/v2/admin/auth/user/logout"
-      stub_request(:post, url)
-        .to_return(status: 200, body: stub_response_body.to_json, headers: {})
-      reps = subject.logout_by_public_address(public_address)
-      expect(reps.status_code).to eq(200)
+    context "#logout_by_issuer" do
+      it "return response" do
+        reps = subject.logout_by_issuer(issuer)
+        expect(reps.status_code).to eq(200)
+      end
     end
-  end
+   
+    context "#logout_by_public_address" do
+      it "return response" do
+        reps = subject.logout_by_public_address(public_address)
+        expect(reps.status_code).to eq(200)
+      end
+    end
 
-  context "#logout_by_token" do
-    it "return response" do
-      url = "https://api.magic.link/v2/admin/auth/user/logout"
-      stub_request(:post, url)
-        .to_return(status: 200, body: stub_response_body.to_json, headers: {})
-      reps = subject.logout_by_token(spec_did_token)
-      expect(reps.status_code).to eq(200)
+    context "#logout_by_token" do
+      it "return response" do
+        reps = subject.logout_by_token(spec_did_token)
+        expect(reps.status_code).to eq(200)
+      end
     end
   end
 end
